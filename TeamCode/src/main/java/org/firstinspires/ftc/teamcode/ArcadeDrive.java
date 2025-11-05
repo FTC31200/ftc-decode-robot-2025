71481fd7e213c7c8e2f42e8b77d0fcbdb9b4f0e5
Ravi Widnef <163608714+StopMotionEGames@users.noreply.github.com>
2025-11-05 14:16:09 -0300

    Update ArcadeDrive.java
    Using OperatorInputs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArcadeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArcadeDrive.java
@@ -15,33 +15,33 @@ public class ArcadeDrive extends OpMode {
     private DcMotor coreHEX;
     private DcMotor intakeMotor;
     private CRServo servo;
-    private boolean canSpinShooter = false;
-    private boolean intakeToggle = false;
-    private boolean prevRightTrigger = false;
+    PIDController pid = new PIDController(.005, 0, 0, .05, telemetry);
 
-    private double shooterSpeed = 1500.0;
-    private final double kP = .005;
-    private final double kI = 0;
-    private final double kD = 0;
-    private final double kF = .05;
-    PIDController pid = new PIDController(kP, kI, kD, kF);
-
+    private OperatorInputs oi;
     @Override
     public void init() {
-
-        // get motors
-        leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
-        rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
-        shooterMotor = hardwareMap.get(DcMotorEx.class, "shooterMotor");
-        coreHEX = hardwareMap.get(DcMotor.class, "coreHEX");
-        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
-        servo = hardwareMap.get(CRServo.class, "servo");
+        oi = new OperatorInputs(gamepad1, telemetry);
+        oi.gamepad = gamepad1;
+        oi.telemetry = telemetry;
 
         // set up motors
+        leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
+        rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
+
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
-        shooterMotor.setDirection(DcMotor.Direction.REVERSE);
-        coreHEX.setDirection(DcMotor.Direction.REVERSE);
+
+        servo = hardwareMap.get(CRServo.class, "servo");
         servo.setDirection(CRServo.Direction.REVERSE);
+
+        coreHEX = hardwareMap.get(DcMotor.class, "coreHEX");
+        coreHEX.setDirection(DcMotor.Direction.REVERSE);
+
+        shooterMotor = hardwareMap.get(DcMotorEx.class, "shooterMotor");
+        shooterMotor.setDirection(DcMotor.Direction.REVERSE);
+        shooterMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        shooterMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
     }
 
     // spinning the CRServo
@@ -53,11 +53,10 @@ public class ArcadeDrive extends OpMode {
     @Override
     public void loop() {
         // movement
-        double drive = -gamepad1.left_stick_y * 0.72;
-        double turn = gamepad1.right_stick_x * 0.85;
 
-        double leftPower = drive + turn;
-        double rightPower = drive - turn;
+
+        double leftPower = oi.drive + oi.turn;
+        double rightPower = oi.drive - oi.turn;
 
         double max = Math.max(Math.abs(leftPower), Math.abs(rightPower));
         if (max > 1.0) {
@@ -68,36 +67,29 @@ public class ArcadeDrive extends OpMode {
         leftMotor.setPower(leftPower);
         rightMotor.setPower(rightPower);
 
-        handleButtonInputs();
-
-        if (canSpinShooter) {
-            double power = pid.calculate(shooterSpeed, shooterMotor.getVelocity());
+        if (oi.isShooterOn) {
+            double power = pid.calculate(oi.shooterSpeed, shooterMotor.getVelocity());
             telemetry.addData("PID Power", power);
+            telemetry.addData("Is at set point", pid.atTargetPoint());
             shooterMotor.setPower(power);
         } else shooterMotor.setPower(0.0);
 
-        if (gamepad1.right_trigger > 0.05) coreHEX.setPower(1.0);
+        // shooting
+        if (oi.shootActive && pid.atTargetPoint()) coreHEX.setPower(1.0);
         else coreHEX.setPower(-1.0);
 
-        if (intakeToggle) intakeMotor.setPower(.9);
-        else intakeMotor.setPower(-.9);
+        if (oi.intakeToggle) intakeMotor.setPower(1);
+        else intakeMotor.setPower(-1);
 
-        prevRightTrigger = gamepad1.left_trigger > 0.05;
+        // inputs
+        oi.update();
 
-        telemetry.addData("Shooter speed", shooterSpeed);
-        telemetry.addData("Shooter State", canSpinShooter ? "On" : "Off");
-        telemetry.addData("Intake State", intakeToggle ? "Pushing" : "Pulling");
+        telemetry.addData("Shooter speed", oi.shooterSpeed);
+        telemetry.addData("Shooter State", oi.isShooterOn ? "On" : "Off");
+        telemetry.addData("is Shooting", oi.shootActive);
+        telemetry.addData("Can shoot", pid.atTargetPoint());
+        telemetry.addData("Intake State", oi.intakeToggle ? "Pushing" : "Pulling");
         telemetry.update();
-    }
 
-    private void handleButtonInputs() {
-        if (gamepad1.dpadUpWasPressed()) canSpinShooter = !canSpinShooter;
-
-        // just for tweaks during tests
-        if (gamepad1.dpadLeftWasPressed()) shooterSpeed -= 50.0;
-        if (gamepad1.dpadRightWasPressed()) shooterSpeed += 50.0;
-        shooterSpeed = Math.max(600.0, Math.min(10000, shooterSpeed));
-
-        if (gamepad1.left_trigger > 0.05 && !prevRightTrigger) intakeToggle = !intakeToggle;
     }
 }
